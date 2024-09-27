a4a448f3e6da30bb7f02a5fcffce51dace2a06cb
zhanhuiping <2413373618@qq.com>
2024-09-27 09:59:01 +0800

    新的用例

--- src/main/java/cn/zhp/InterfaceExample.java
+++ src/main/java/cn/zhp/InterfaceExample.java
@@ -10,4 +10,16 @@ package cn.zhp;
  * @Version 1.0
  */
 public interface InterfaceExample {
+    public static final int m=10;
+
+    public abstract void fun();
+
+    default void printFun(){
+        System.out.println("这是一个接口的默认实现方法！");
+    }
+    public static void fun2(){
+        System.out.println("这是一个静态方法！");
+    }
+
+
 }
--- src/main/java/cn/zhp/p2/AbstractClass.java
+++ src/main/java/cn/zhp/p2/AbstractClass.java
@@ -1,5 +1,7 @@
 package cn.zhp.p2;
 
+import com.sun.xml.internal.ws.api.streaming.XMLStreamWriterFactory;
+
 /**
  * ClassName: AbstractClass
  * Package: cn.zhp.p2
@@ -10,4 +12,12 @@ package cn.zhp.p2;
  * @Version 1.0
  */
 public abstract class AbstractClass {
+
+    public static final int a=10;
+
+    public abstract void fun1();
+
+    public void print(){
+        System.out.println("这是抽象类中的一个普通方法！");
+    }
 }
